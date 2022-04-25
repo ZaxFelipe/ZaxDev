5a7c125d7b1085615a6ad12c376c6629c081815d
ZaxFelipe <78233720+ZaxFelipe@users.noreply.github.com>
2022-04-25 15:57:58 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function abrirDicionario(){
+    let element = document.querySelector("#button1")
+    if (element.innerText == ":D"){
+        element.innerText = "D:"
+    } else {element.innerText = ":D"}
+    
+
+}
